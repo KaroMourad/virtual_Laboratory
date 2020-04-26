6a145b1edcf38fafc2c3014cf16d80249c3f2e9e
Karapet Mouradyan <karapetmouradyan@gmail.com>
2020-04-26 15:07:25 +0400

    min width

--- src/components/BallsMovement/graphic.js
+++ src/components/BallsMovement/graphic.js
@@ -70,72 +70,74 @@ const Graphic = ({timeStart, velocity1, velocity2, initialDelta1, restart, init}
     return (
         <div className={styles.graphic}>
             <div style={{paddingLeft: 20, height: 30, display: "flex", alignItems: "flex-end"}}>V(t) {toggle} S(t)</div>
-            <ResponsiveContainer width="90%" height="90%">
-                <AreaChart data={data} margin={{top: 25, right: 10, bottom: 0, left: -15}}>
-                    <defs>
-                        <linearGradient id={id1} x1="0" y1="0" x2="0" y2="1">
-                            <stop offset="5%" stopColor={color1} stopOpacity={0.8}/>
-                            <stop offset="95%" stopColor={color1} stopOpacity={0}/>
-                        </linearGradient>
-                    </defs>
-                    <defs>
-                        <linearGradient id={id2} x1="0" y1="0" x2="0" y2="1">
-                            <stop offset="5%" stopColor={color2} stopOpacity={0.8}/>
-                            <stop offset="95%" stopColor={color2} stopOpacity={0}/>
-                        </linearGradient>
-                    </defs>
-                    <defs>
-                        <linearGradient id={id3} x1="0" y1="0" x2="0" y2="1">
-                            <stop offset="5%" stopColor={color3} stopOpacity={0.8}/>
-                            <stop offset="95%" stopColor={color3} stopOpacity={0}/>
-                        </linearGradient>
-                    </defs>
-                    <XAxis
-                        label="t"
-                        strokeWidth="0.5px"
-                        stroke="#6a8fb7"
-                        dataKey="t"
-                        type="number"
-                        interval={0}
-                        domain={[0, 6]}
-                        tick={<CustomizedAxisTick axis="x"/>}
-                    />
-                    <YAxis
-                        type="number"
-                        domain={[-Math.ceil(initialDelta1 + (checked ? 2 : 0)), Math.ceil(initialDelta1 + (checked ? 2 : 0))]}
-                        strokeWidth="0.5px"
-                        stroke="#6a8fb7"
-                        interval={0}
-                        tick={<CustomizedAxisTick axis="y"/>}
-                    />
-                    <CartesianGrid strokeDasharray="1 1" strokeWidth="0.5px"/>
-                    <Tooltip labelFormatter={(value) => `t: ${value} s`}/>
-                    <ReferenceLine y={0} stroke="red"/>
-                    <ReferenceLine y={checked ? data[data.length - 1]?.S : initialDelta1} stroke="blue"/>
-                    <ReferenceLine x={data[data.length - 1]?.t || 0} stroke="blue"/>
-                    <Area
-                        type="monotone"
-                        dataKey="S"
-                        stroke={color3}
-                        fillOpacity={1}
-                        fill={`url(#${id3})`}
-                    />
-                    <Area
-                        type="monotone"
-                        dataKey="V1"
-                        stroke={color1}
-                        fillOpacity={1}
-                        fill={`url(#${id1})`}
-                    />
-                    <Area
-                        type="monotone"
-                        dataKey="V2"
-                        stroke={color2}
-                        fillOpacity={1}
-                        fill={`url(#${id2})`}
-                    />
-                </AreaChart>
-            </ResponsiveContainer>
+            <div style={{minWidth: 320, minHeight: 300, width: "90%", height: "90%"}}>
+                <ResponsiveContainer width="100%" height="100%">
+                    <AreaChart data={data} margin={{top: 25, right: 10, bottom: 0, left: -15}}>
+                        <defs>
+                            <linearGradient id={id1} x1="0" y1="0" x2="0" y2="1">
+                                <stop offset="5%" stopColor={color1} stopOpacity={0.8}/>
+                                <stop offset="95%" stopColor={color1} stopOpacity={0}/>
+                            </linearGradient>
+                        </defs>
+                        <defs>
+                            <linearGradient id={id2} x1="0" y1="0" x2="0" y2="1">
+                                <stop offset="5%" stopColor={color2} stopOpacity={0.8}/>
+                                <stop offset="95%" stopColor={color2} stopOpacity={0}/>
+                            </linearGradient>
+                        </defs>
+                        <defs>
+                            <linearGradient id={id3} x1="0" y1="0" x2="0" y2="1">
+                                <stop offset="5%" stopColor={color3} stopOpacity={0.8}/>
+                                <stop offset="95%" stopColor={color3} stopOpacity={0}/>
+                            </linearGradient>
+                        </defs>
+                        <XAxis
+                            label="t"
+                            strokeWidth="0.5px"
+                            stroke="#6a8fb7"
+                            dataKey="t"
+                            type="number"
+                            interval={0}
+                            domain={[0, 6]}
+                            tick={<CustomizedAxisTick axis="x"/>}
+                        />
+                        <YAxis
+                            type="number"
+                            domain={[-Math.ceil(initialDelta1 + (checked ? 2 : 0)), Math.ceil(initialDelta1 + (checked ? 2 : 0))]}
+                            strokeWidth="0.5px"
+                            stroke="#6a8fb7"
+                            interval={0}
+                            tick={<CustomizedAxisTick axis="y"/>}
+                        />
+                        <CartesianGrid strokeDasharray="1 1" strokeWidth="0.5px"/>
+                        <Tooltip labelFormatter={(value) => `t: ${value} s`}/>
+                        <ReferenceLine y={0} stroke="red"/>
+                        <ReferenceLine y={checked ? data[data.length - 1]?.S : initialDelta1} stroke="blue"/>
+                        <ReferenceLine x={data[data.length - 1]?.t || 0} stroke="blue"/>
+                        <Area
+                            type="monotone"
+                            dataKey="S"
+                            stroke={color3}
+                            fillOpacity={1}
+                            fill={`url(#${id3})`}
+                        />
+                        <Area
+                            type="monotone"
+                            dataKey="V1"
+                            stroke={color1}
+                            fillOpacity={1}
+                            fill={`url(#${id1})`}
+                        />
+                        <Area
+                            type="monotone"
+                            dataKey="V2"
+                            stroke={color2}
+                            fillOpacity={1}
+                            fill={`url(#${id2})`}
+                        />
+                    </AreaChart>
+                </ResponsiveContainer>
+            </div>
         </div>
     );
 };
